07d6f0202a00e8d50326acf392dfba6affe1b068
Connor <connor-home@shasta.local>
2020-06-29 11:15:08 -0700

    removing unnecessary files

--- test1026.m
+++
@@ -1,9 +0,0 @@
-function test1026(a, varargin)
-disp(a+10)
-iscell(varargin)
-for ii = 1:length(varargin)
-    disp(varargin)
-    disp(varargin{ii})
-
-end
-end
--- tfind_test.m
+++
@@ -1,8 +0,0 @@
-xl = xlsread('L:/iEEG_San_Diego/Functions/iEEG_SD_pts.xlsx', 2, 'H2:H118');
-msk = zeros(2,length(evns)); 
-for ii = 1:length(xl)
-    a = evns == xl(ii);
-    msk = msk + a;
-end
- sum(msk(1,:))
- sum(msk(2,:))
